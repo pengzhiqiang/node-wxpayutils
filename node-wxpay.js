21cc286c23214df75465acb6eaf6b4d5b492d1e5
pengzhiqiang <742760399@qq.com>
2017-08-10 14:48:15 +0800

    up

---
+++ node-wxpay.js
@@ -0,0 +1,53 @@
+const crypto = require('crypto');
+
+module.exports = {
+    //MD5
+    MD5: (string) => {
+        return crypto.createHash('md5').update(string).digest('hex');
+    },
+    
+    //生成签名(sign)
+    createSign: (obj, key) => {
+        let keyArr = [];
+        for (let key in obj) {
+            keyArr.push(key)
+        }
+        keyArr.sort();
+        let stringA = '';
+        for (let index in keyArr) {
+            let len = keyArr.length;
+            stringA += keyArr[index] + "=" + obj[keyArr[index]] + '&'
+        }
+        let stringB = stringA + "key=" + key;
+        return MD5(stringB).toUpperCase();
+    },
+
+    //生成随机串(nonce_str)
+    createRandomStr: (len) => {
+        let strBase = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let randomStr = '';
+        for (let i = 0; i < len; i++) {
+            let index = Math.floor(Math.random() * 52);
+            let strBaseArr = strBase.split("");
+            if (strBaseArr[index]) {
+                randomStr += strBaseArr[index]
+            } else {
+                randomStr += 0
+            }
+        }
+        return randomStr;
+    },
+
+    //json转XML
+    createXMLStr: (jsonObj) => {
+        let items = [];
+        let xml = '';
+        for (let item in jsonObj) {
+            let curVal = jsonObj[item];
+            items.push("<" + item + ">" + curVal + "</" + item + ">");
+        }
+        xml = "<xml>" + items.join("") + "</xml>";
+        return xml;
+    }
+
+}
